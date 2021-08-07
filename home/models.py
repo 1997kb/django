fce39149154ea4d9fb96215af04618476a86222e
1997kb <71140277+1997kb@users.noreply.github.com>
2021-08-07 12:07:05 +0545

    workout with models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# add data in admin panel
+
+class Contact(models.Model):
+	name = models.CharField(max_length = 300)
+	email = models.CharField(max_length = 300)
+	subject = models.TextField()
+	message = models.TextField()
+
+	def __str__(self):
+		return self.name
+# to fetch data from database
+class Info(models.Model):
+	address = models.CharField(max_length=300)
+	local_address = models.CharField(max_length=400)
+	email = models.CharField(max_length=300)
+	time = models.CharField(max_length=400)
+	phone = models.CharField(max_length=100,blank = True) #we need to add this if are adding a new field to this existing model
+
+
+	def __str__(self):
+		return self.address
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 def home(request):
 	return render(request,'index.html')
@@ -7,7 +8,24 @@ def about(request):
 	return render(request,'about.html')
 
 def contact(request):
-	return render(request,'contact.html')
+	if request.method == "POST": #form ma pathako value fetch garya ho
+		name = request.POST['name']
+		email = request.POST['email']
+		subject = request.POST['subject']
+		message = request.POST['message']
+		data = Contact.objects.create(
+				name = name,
+				email = email,
+				subject = subject,
+				message = message
+
+			)
+		data.save()
+	
+	views  = {}
+	views['infos'] = Info.objects.all() # this means select all from info model
+
+	return render(request,'contact.html',views)
 
 def portfolio(request):
 	return render(request,'portfolio.html')	
